7692740d750f0db9cbfdd032f1e7ebe595969e96
countviolencia <countviolencia@gmail.com>
2016-06-27 12:59:38 +0300

    method chining is users

--- Users.php
+++ Users.php
@@ -1,13 +1,24 @@
 <?php
 
 //Your practice code
- class Users {
+ class User {
      public $firstName;
      public $lastName;
      public $emailAddress;
      
      public function hello($firstName, $lastName){
          return "Hello $firstName $lastName welcome to life";
+     }
+     
+     public function register(){
+         echo $this->firstName . " " . $this->lastName ." registered";
+         
+         return $this;
+     }
+     
+     public function mail(){
+         echo " emailed";
+         
      }
      
      public function verifyEmail(){
@@ -17,11 +28,17 @@
      
  }
  
- $users = new Users();
- $welcome =  $users -> hello('Hezzy', 'Okello');
+ $user = new User();
+ $welcome =  $user -> hello('Hezzy', 'Okello');
  echo $welcome;
  echo "<br />";
-$users->emailAddress = "ronnixo@gmail.com";
-echo $users->verifyEmail();
+$user->emailAddress = "ronnixo@gmail.com";
+echo $user->verifyEmail();
+echo "<hr />";
+
+$user1 = new User();
+$user1 -> firstName = "Jane";
+$user1 -> lastName = "Roe";
  
+ echo $user1 ->register()->mail();
  ?>
